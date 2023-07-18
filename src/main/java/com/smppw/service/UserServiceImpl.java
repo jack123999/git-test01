8f06cc05a48f1d900f33b1a7556ebf4bed4dff02
zhoufan <742565508@qq.com>
2023-07-19 00:56:56 +0800

    添加add接口

--- src/main/java/com/smppw/service/UserServiceImpl.java
+++ src/main/java/com/smppw/service/UserServiceImpl.java
@@ -3,5 +3,8 @@ package com.smppw.service;
 public class UserServiceImpl {
     public void add() {
         System.out.println("hello world!!!");
+        System.out.println("xxkkooo!!!");
+        System.out.println("sdsdsxxx");
+        System.out.println("oaoal!!!");
     }
 }
