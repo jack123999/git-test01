2ffb8b4ff9c106bb65e4e34059b4ee89d1a3144d
zhoufan <742565508@qq.com>
2023-07-19 00:58:31 +0800

    账户接口添加update方法

--- src/main/java/com/smppw/service/AccountServiceImpl.java
+++ src/main/java/com/smppw/service/AccountServiceImpl.java
@@ -4,4 +4,7 @@ public class AccountServiceImpl {
     public void addAccount() {
         System.out.println("hello world....");
     }
+    public void upDateAccount() {
+        System.out.println("updateAccount...");
+    }
 }
