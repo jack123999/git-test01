01857e4262d492efaeea01a26b4b81d4c33c9d25
jack123999 <94337304+jack123999@users.noreply.github.com>
2023-07-19 00:56:36 +0800

    Update AccountServiceImpl.java
    添加账户附加信息

--- src/main/java/com/smppw/service/AccountServiceImpl.java
+++ src/main/java/com/smppw/service/AccountServiceImpl.java
@@ -3,5 +3,6 @@ package com.smppw.service;
 public class AccountServiceImpl {
     public void addAccount() {
         System.out.println("hello world....");
+        System.out.println("hello world1111....");
     }
 }
